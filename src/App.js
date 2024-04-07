a32b7d545991a3b29dc2d47dbef89a041f8530db
erica <erica.sanchez2835@yahoo.com>
2024-04-06 19:45:10 -0500

    all pages working and added views

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { HashRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import { Contact } from './components/contact/Contact';
+import { Home } from './components/home/Home';
+import { ToDo } from './components/to-do/ToDo';
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="header">
+            <h1>what's next?</h1>
+            <Router>
+              <ul>
+                <li>
+                  <Link to="/home">about</Link>
+                </li>
+                <li>
+                  <Link to="/to-do">to-do's</Link>
+                </li>
+                <li>
+                  <Link to="/contact">contact</Link>
+                </li>
+              </ul>
+              <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/to-do" element={<ToDo />} />
+                <Route path="/contact" element={<Contact />} />
+              </Routes>
+            </Router>
+      </header>
     </div>
   );
 }
--- src/components/to-do/ToDoWrapper.js
+++ src/components/to-do/ToDoWrapper.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const ToDoWrapper = () => {
   return (
-    <div>ToDoWrapper</div>
+    <div>
+        
+    </div>
   )
 }
