baafc5e29065c60c7cb2428e6531f93bf422bd70
erica <erica.sanchez2835@yahoo.com>
2024-04-07 17:22:36 -0500

    homepage complete

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faListCheck } from '@fortawesome/free-solid-svg-icons';
 
 export const Home = () => {
   return (
     <div className="homepage">
         <div className="home1">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Quidem repudiandae maiores molestiae dignissimos eos dicta vel, 
-                doloribus quos. Quas corrupti esse corporis totam hic adipisci iste.
-            </p>
+          <h2>first on your list: </h2>
+            <p>1. lorem ipsum dolor sit amet consecte dictas </p>
+            <p>2. blanditiis, aliquam</p>
+            <p>3. orem ipsum dolor  elit</p>
+            
         </div>
         <div className="home2">
-            <p>Ready to get started? Begin your to-do list today!</p>
+          <FontAwesomeIcon icon={faListCheck} />
+            <p>take your needs to the next level</p>
+            <p>begin your to-do list today</p>
             <Link to="/to-do">
-              <button type="button">create list</button>
+              <button type="button" className="grad-btn">create list</button>
             </Link>
         </div>
     </div>
